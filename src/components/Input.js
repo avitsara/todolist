3bd3be1e398666534fa7897686d421dd45b57ee1
avitsara <avitsarajary@yahoo.com>
2021-06-04 19:14:17 +0200

    feat: creation of input element

---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+class Input extends React.Component {
+    render() {
+      return <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
+  }
+}
+
+export default Input;
